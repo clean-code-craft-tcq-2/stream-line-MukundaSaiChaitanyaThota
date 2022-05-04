5f63e9c9d54dd893ad1fb743ca4cad2cfdecf5d6
MukundaSaiChaitanyaThota <91048236+MukundaSaiChaitanyaThota@users.noreply.github.com>
2022-05-04 10:39:55 +0530

    Update receiver.test.py

--- receiver.test.py
+++ receiver.test.py
@@ -9,7 +9,7 @@ class RcevierTest(unittest.TestCase):
         self.assertTrue(receiver.getindex("temperature") == 1)
     
     def test_extractEachParameterReadings(self):
-        self.assertTrue(receiver.extractEachParameterReadings([[25,13],[6,15]], "SOC") == [5,6])
+        self.assertTrue(receiver.extractEachParameterReadings([[25,13],[6,15]], "SOC") == [25,6])
         self.assertTrue(receiver.extractEachParameterReadings([[6,12],[6,35]], "temperature") == [12,35])      
         
     def test_calculateMinMaxParameter(self):
@@ -19,13 +19,13 @@ class RcevierTest(unittest.TestCase):
         self.assertTrue(receiver.calculateMinMaxReading([7,15,5,3,10]) == {'min': 3, 'max': 15})
     
     def test_calculateMovingAverage(self):
-        self.assertTrue(receiver.calculateMovingAverage([8,12,4,5,11,19,16,14], 5) == [8.0,9.6,10.4,12.4])
+        self.assertTrue(receiver.calculateMovingAverage([8,12,4,5,11,19,16,14], 5) == [8.0,10.2,11,13])
     
     def test_createRangeWindow(self):
         self.assertTrue(receiver.createRangeWindow([8,12,4,5,11,19,16,14], 5) == [[8,12,4,5,11], [12,4,5,11,19], [4,5,11,19,16], [5,11,19,16,14]])
 
-    def test_formulateReadings(self):
-        self.assertTrue(receiver.formulateReadings(['5,10\n','6,15\n']) == [[5,10],[6,15]])
+    def test_processInput(self):
+        self.assertTrue(receiver.processInput(['5,10\n','6,15\n']) == [[5,10],[6,15]])
 
 if __name__ == '__main__': # pragma: no cover
   unittest.main()
